5445cfbd5a1f0d6984f986cf967fedf964abb43a
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-10-05 15:15:29 +0300

    add order

--- order/views.py
+++ order/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views.generic import CreateView
 
-# Create your views here.
+from boat.models import Boat
+from order.models import Order
+
+
+class OrderCreateView(CreateView):
+    model = Order
+    fields = ('name', 'email', 'message',)
+
+    def get_success_url(self):
+        return reverse('boat:boat_detail', args=[self.kwargs.get('pk')])
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['boat'] = get_object_or_404(Boat, pk=self.kwargs.get('pk'))
+        return context_data
